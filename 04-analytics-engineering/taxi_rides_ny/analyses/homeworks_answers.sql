9a57de94bd57a714b39ae86dd10fd1793d4a3612
Lukasz Wlodarczyk <lukasz@pushmetrics.io>
2026-02-16 01:07:17 +0100

    feat: update answers

--- 04-analytics-engineering/taxi_rides_ny/analyses/homeworks_answers.sql
+++ 04-analytics-engineering/taxi_rides_ny/analyses/homeworks_answers.sql
@@ -1,7 +1,7 @@
-3.
+--3.
 select count(*) from prod.fct_monthly_zone_revenue;
 
-4.
+--4.
 select pickup_zone, sum(revenue_monthly_total_amount) as total_revenue
 from prod.fct_monthly_zone_revenue
 where revenue_month >= '2020-01-01' and revenue_month < '2021-01-01'
@@ -10,8 +10,11 @@ group by 1
 order by 2 desc
 limit 5;
 
-5.
+--5.
 select sum(total_monthly_trips) as total_monthly_trips
 from prod.fct_monthly_zone_revenue
 where revenue_month = '2019-10-01'
 and service_type = 'Green';
+
+--6.
+select count(*) from prod.stg_fhv_tripdata;
